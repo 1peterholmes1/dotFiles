8f294bb1fc8ff2283af6bbd8c8234644dfe0b7bc
Peter Holmes <peter@beckenterprises.com>
2024-03-26 12:41:32 -0500

    enabled undotree mapping

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -26,7 +26,7 @@ return {
     {
         'mbbill/undotree',
         config = function()
-            vim.keymap.set('n', '<leader><F5>', vim.cmd.UndotreeToggle)
+            vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
         end,
         event = "BufEnter"
     }
