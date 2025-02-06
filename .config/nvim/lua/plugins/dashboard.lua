9414097a323e0e79e6b9a39c982460529b5755e2
Peter Holmes <peter@beckenterprises.com>
2025-02-06 10:34:04 -0600

    more dashboard shortcuts

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -22,10 +22,24 @@ return {
                     project = { enable = false },
                     mru = { cwd_only = true },
                     shortcut = {
-                        { desc = 'Find Files ',  group = 'Label', action = 'Telescope find_files', key = 'f' },
-                        { desc = 'Toggle Tree ', group = 'Label', action = 'Neotree toggle',       key = 'e' },
-                        { desc = 'Quit ',        group = 'Label', action = 'q',                    key = 'q' },
-                        { desc = 'Find Word ',   group = 'Label', action = 'Telescope live_grep',  key = 'w' },
+                        { desc = 'Search Files ', group = 'Label', action = 'Telescope find_files', key = 'f' },
+                        { desc = 'Search All Files ', group = 'Label', action = function()
+                            local builtin = require('telescope.builtin')
+                            builtin.find_files { hidden = true, no_ignore = true }
+                        end
+                        , key = 'F' },
+                        { desc = 'Find Word ',    group = 'Label', action = 'Telescope live_grep',  key = 'w' },
+                        { desc = 'Find Word Everywhere ', group = 'Label', action = function()
+                            local builtin = require('telescope.builtin')
+                            builtin.live_grep {
+                                additional_args = function(args)
+                                    return vim.list_extend(args,
+                                        { "--hidden", "--no-ignore" })
+                                end
+                            }
+                        end, key = 'W' },
+                        { desc = 'Toggle Tree ', group = 'Label', action = 'Neotree toggle', key = 'e' },
+                        { desc = 'Quit ',        group = 'Label', action = 'q',              key = 'q' },
                     },
                     footer = {}
                 }
